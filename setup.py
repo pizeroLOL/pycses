a0676321c6fb166e1de7f0e9232bf0889f7f7c44
unDefFtr <undefftr@undefined.ac.cn>
2025-02-01 13:36:31 +0800

    [FEAT] 修改setup.py 满足打包要求

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='unDefFtr',
     author_email='undefftr@undefined.ac.cn',
     description='CSES access framework for Python',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding="utf-8").read(),
     long_description_content_type='text/markdown',
     url='https://github.com/CSES-org/pycses',
     packages=find_packages(),
