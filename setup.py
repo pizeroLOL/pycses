b51a94127d940a5f8633a9f511d074c92f9918b7
unDefFtr <undefftr@undefined.ac.cn>
2025-02-01 14:21:04 +0800

    Modify setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pycses',
-    version='0.1',
+    version='0.1.2',
     author='unDefFtr',
     author_email='undefftr@undefined.ac.cn',
     description='CSES access framework for Python',
